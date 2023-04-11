5ad236272b137b982a48ce540a0f171804d3adde
FikG-ui <130470326+FikG-ui@users.noreply.github.com>
2023-04-11 22:09:33 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -35,7 +35,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-04-05' },
+        { keyword: 'love_day', date: '2020-04-05' },
         // 订婚纪念日
         { keyword: 'marry_day', date: '2023-01-07' },
       ],
